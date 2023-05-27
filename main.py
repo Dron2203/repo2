206aa6cbc9ebcbfa6806c9f91fb4b63d67e5858f
Dron <dron2203@gmail.com>
2023-05-27 22:59:02 +0300

    Добавил работу с БД

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
+import pymysql
+
+
 def read_data_file(file_name):
     file_lines = []
     file = open(file_name, 'r')
     for line in file:
         file_lines.append(line)
     return file_lines
+
+
 def split_line(line):
     splited_line = line.split(';')
     return splited_line
 
 
+def connect_to_db(host, port, db_name, user, password):
+    connection = pymysql.connect(host=host, user=user, port=port, password=password, db=db_name)
+    cursor = connection.cursor()
+    return cursor, connection
+
 if __name__ == '__main__':
+    cursor, connection = connect_to_db('192.168.1.87',3306,'test','student','123456')
     stroki_iz_faila = read_data_file('data.csv')
     for stroka in stroki_iz_faila:
         razbitaya_stroka = split_line(stroka)
-        #print(razbitaya_stroka)
-        print(f'Меня зовут {razbitaya_stroka[0]} {razbitaya_stroka[1]}, пол {razbitaya_stroka[2]}, год рождения {razbitaya_stroka[3]}, мой номер {razbitaya_stroka[4]}')
-
+        cursor.execute("insert into sprav values('{}','{}','{}','{}','{}');".format(razbitaya_stroka[0],razbitaya_stroka[1],razbitaya_stroka[2],razbitaya_stroka[3],razbitaya_stroka[4].strip()))
+    connection.commit()
